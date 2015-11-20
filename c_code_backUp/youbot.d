45f7f0af25e2d3eb9eac2cf424621a71a62697bd
Pulin Agrawal <pulinagrawal@github.com>
2015-11-19 18:29:20 -0600

    removed backup folders

--- c_code_backUp/youbot.d
+++
@@ -1,6 +0,0 @@
-youbot.o: youbot.c \
- C:\Program\ Files\ (x86)\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\ (x86)\Webots/include/controller/c/webots/types.h \
- ../../lib/base.h \
- C:\Program\ Files\ (x86)\Webots/include/controller/c/webots/types.h \
- ../../lib/arm.h ../../lib/gripper.h
