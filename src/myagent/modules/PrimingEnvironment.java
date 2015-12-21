cdcefdcccd9a2a2fd938e35ec923b815ca561008
Pulin Agrawal <pulinagrawal@users.noreply.github.com>
2015-12-21 12:39:02 +0530

    environment module complete.

--- src/myagent/modules/PrimingEnvironment.java
+++ src/myagent/modules/PrimingEnvironment.java
@@ -4,28 +4,36 @@ import java.util.HashMap;
 import java.util.Map;
 
 import edu.memphis.ccrg.lida.environment.EnvironmentImpl;
+import edu.memphis.ccrg.lida.framework.tasks.TaskManager;
 
 public class PrimingEnvironment extends EnvironmentImpl{
 
-    private Map<String, Object> sensedData = new HashMap<String, Object>();
-
+    private Map<String, Object> blankData= new HashMap<String, Object>();
     private Map<String, Object> targetData= new HashMap<String, Object>();
     private Map<String, Object> consistentPrimingData= new HashMap<String, Object>();
     private Map<String, Object> unconsistentPrimingData= new HashMap<String, Object>();
+    
+    private int blankDuration=0;
 
 	@Override
 	public void init(){
+	blankDuration=getParam("blank_duration", 10);
+
+	blankData.put("dot_color","white");
+	blankData.put("dot_Xpos", 1);
+	blankData.put("dot_Ypos", 1);
+
 	targetData.put("annulus1_color", "red");	
 	targetData.put("annulus2_color", "green");	
-	targetData.put("annulus1_Xpos", 1);
-	targetData.put("annulus1_Ypos", 1);
+	targetData.put("annulus1_Xpos", 2);
+	targetData.put("annulus1_Ypos", 2);
 	targetData.put("annulus2_Xpos", 0);
 	targetData.put("annulus2_Ypos", 0);
 
 	consistentPrimingData.put("circle1_color", "red");	
 	consistentPrimingData.put("circle2_color", "green");	
-	consistentPrimingData.put("circle1_Xpos", 1);
-	consistentPrimingData.put("circle1_Ypos", 1);
+	consistentPrimingData.put("circle1_Xpos", 2);
+	consistentPrimingData.put("circle1_Ypos", 2);
 	consistentPrimingData.put("circle2_Xpos", 0);
 	consistentPrimingData.put("circle2_Ypos", 0);
 
@@ -33,15 +41,31 @@ public class PrimingEnvironment extends EnvironmentImpl{
 	unconsistentPrimingData.put("circle2_color", "green");	
 	unconsistentPrimingData.put("circle1_Xpos", 0);
 	unconsistentPrimingData.put("circle1_Ypos", 0);
-	unconsistentPrimingData.put("circle2_Xpos", 1);
-	unconsistentPrimingData.put("circle2_Ypos", 1);
+	unconsistentPrimingData.put("circle2_Xpos", 2);
+	unconsistentPrimingData.put("circle2_Ypos", 2);
 
 	}
 
 	@Override
 	public Object getState(Map<String, ?> arg0) {
 
-		return sensedData;
+		if(TaskManager.getCurrentTick()<700){
+			return blankData;
+		}
+		else if(TaskManager.getCurrentTick()>700 && TaskManager.getCurrentTick()<(10+700)){
+			if(getParam("consistent", true)){
+			 return consistentPrimingData;	
+			}
+			else{
+				return unconsistentPrimingData;
+			}
+		}
+		else if(TaskManager.getCurrentTick()>(10+700) && TaskManager.getCurrentTick()<(blankDuration+10+700)){
+			return blankData;
+		}
+		else{
+			return targetData;
+		}
 	}
 
 	@Override
